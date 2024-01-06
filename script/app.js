09af24e5069b4561f99b92d71eff59b110ce9a1f
astron-out <cosmos.4618282@gmail.com>
2024-01-06 21:57:48 +0500

    Something is updated

---
+++ script/app.js
@@ -0,0 +1,13 @@
+// const popupBg = document.getElementById("popup");
+// const popupContent = document.getElementById("popup__content");
+
+// function popupBgClose() {
+//   popupBg.classList.add("popup-close-pop");
+// }
+
+// function popupContentClose() {
+//   popupContent.classList.add("popup-close-contet");
+// }
+
+// popupBg.addEventListener("click", popupBgClose);
+// popupContent.addEventListener("click", popupContentClose);
